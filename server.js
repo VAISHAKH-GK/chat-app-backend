558496f590f831236a3928dee6af69ea2c4fddd7
VAISHAKH GK <vaishakhgk2006@gmail.com>
2022-06-20 13:09:56 +0530

    creating server in www instead of creating in www

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { createServer } = require("http");
 const db = require("./configs/mongodb");
 const bodyParser = require("body-parser");
 const session = require("express-session");
@@ -7,15 +6,9 @@ const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const userRouter = require("./routes/api/user");
 const messageRouter = require("./routes/api/message");
-const io = require("./configs/socketIo");
 
-const port = process.env.PORT;
 const fiveMinute = 1000 * 60 * 5;
 const app = express();
-
-const server = createServer(app);
-
-io.attach(server);
 
 app.use(cookieParser());
 
@@ -47,7 +40,5 @@ db.connect((err) => {
 app.use('/api/user', userRouter);
 app.use('/api/message', messageRouter);
 
-server.listen(port, () => {
-  console.log("server started");
-});
 
+module.exports = app;
